eb4db604b32fab1783cfae001cbeb5f4e71c4316
Mehdi Salimi <Salimi.devop@gmail.com>
2023-02-13 03:08:16 +0330

    feat:created Topbar section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ColorModeContext, useMode } from './theme';
 import { CssBaseline, ThemeProvider } from '@mui/material';
+import Topbar from './scenes/global/Topbar';
 
 const App = () => {
     const [theme, colorMode] = useMode();
@@ -9,7 +10,9 @@ const App = () => {
             <ThemeProvider theme={theme}>
                 <CssBaseline />
                 <div className='app'>
-                    <main className='content'></main>
+                    <main className='content'>
+                        <Topbar />
+                    </main>
                 </div>
             </ThemeProvider>
         </ColorModeContext.Provider>
