3ea368d1844546ca4b8806d3ec6609f91c6c83c3
Mehdi Salimi <Salimi.devop@gmail.com>
2023-02-26 22:08:51 +0330

    feat:form validation added with using yup package

--- src/scenes/form/index.jsx
+++ src/scenes/form/index.jsx
@@ -2,6 +2,7 @@ import { Box, Button, TextField } from '@mui/material';
 import { Formik } from 'formik';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import Header from '../../components/Header';
+import * as yup from 'yup';
 
 const Form = () => {
     const isNonMobile = useMediaQuery('(min-width:600px)');
@@ -15,6 +16,16 @@ const Form = () => {
         address2: '',
     };
 
+    const phoneRegExp = /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
+    const checkoutSchema = yup.object().shape({
+        firstName: yup.string().required('required'),
+        lastName: yup.string().required('required'),
+        email: yup.string().email('invalid email').required('required'),
+        contact: yup.string().matches(phoneRegExp, 'Phone number is not valid').required('required'),
+        address1: yup.string().required('required'),
+        address2: yup.string().required('required'),
+    });
+
     const handleFormSubmit = values => {
         console.log(values);
     };
@@ -23,7 +34,7 @@ const Form = () => {
         <Box m='20px'>
             <Header title='CREATE USER' subtitle='Create a New User Profile' />
 
-            <Formik onSubmit={handleFormSubmit} initialValues={initialValues}>
+            <Formik onSubmit={handleFormSubmit} initialValues={initialValues} validationSchema={checkoutSchema}>
                 {({ values, errors, touched, handleBlur, handleChange, handleSubmit }) => (
                     <form onSubmit={handleSubmit}>
                         <Box
