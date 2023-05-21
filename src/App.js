e3dda0711aa626f2384fea82e8a0046db9d3ec0a
Mehdi Salimi <Salimi.devop@gmail.com>
2023-05-22 01:28:38 +0330

    create faq page

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Contacts from './scenes/contacts';
 import Invoices from './scenes/invoices';
 import Form from './scenes/form';
 import Calendar from './scenes/calendar/calendar';
+import FAQ from './scenes/faq';
 
 const App = () => {
     const [theme, colorMode] = useMode();
@@ -27,7 +28,8 @@ const App = () => {
                             <Route path='/contacts' element={<Contacts />} />
                             <Route path='/invoices' element={<Invoices />} />
                             <Route path='/form' element={<Form />} />
-                            <Route path="/calendar" element={<Calendar />} />
+                            <Route path='/calendar' element={<Calendar />} />
+                            <Route path='/faq' element={<FAQ />} />
                         </Routes>
                     </main>
                 </div>
