bba41559c2a6a54471d93d67991d38a386849b57
Mehdi Salimi <Salimi.devop@gmail.com>
2023-05-21 22:09:08 +0330

    feat:add calender page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Team from './scenes/team';
 import Contacts from './scenes/contacts';
 import Invoices from './scenes/invoices';
 import Form from './scenes/form';
+import Calendar from './scenes/calendar/calendar';
 
 const App = () => {
     const [theme, colorMode] = useMode();
@@ -26,6 +27,7 @@ const App = () => {
                             <Route path='/contacts' element={<Contacts />} />
                             <Route path='/invoices' element={<Invoices />} />
                             <Route path='/form' element={<Form />} />
+                            <Route path="/calendar" element={<Calendar />} />
                         </Routes>
                     </main>
                 </div>
